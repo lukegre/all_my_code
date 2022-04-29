9df98d002a6d171294ef6a258cedec2c05a9e5eb
lukegre <lukegre@gmail.com>
2022-04-29 10:58:32 +0200

    moved zip_folder from files.utils to files.save

--- all_my_code/files/save.py
+++ all_my_code/files/save.py
@@ -1,6 +1,3 @@
-from pkgutil import get_data
-from functools import wraps
-from numpy import isin
 import xarray as xr
 
 
@@ -10,7 +7,14 @@ class Save(object):
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
 
-    def __call__(self, sname, overwrite=False, compression='int16', max_percentile=99.999, **kwargs):
+    def __call__(
+        self,
+        sname,
+        overwrite=False,
+        compression="int16",
+        max_percentile=99.999,
+        **kwargs,
+    ):
         """
         Save data to a netCDF file with compression.
 
@@ -21,13 +25,13 @@ class Save(object):
         overwrite : bool [False]
             Overwrite the file if it already exists.
         compression : str [int16]
-            The type of compression to use. Options are 
+            The type of compression to use. Options are
                 - `int16` (lossy)
                 - `zip` (lossless)
                 - `none` (no compression)
         **kwargs : dict
             Additional keyword arguments to pass to `xarray.Dataset.to_netcdf`.
-        
+
         Returns
         -------
         sname : str
@@ -36,42 +40,43 @@ class Save(object):
         import os
 
         ds = self._obj
-        
+
         if isinstance(ds, xr.DataArray):
             try:
                 ds = ds.to_dataset()
-            except ValueError as E:
+            except ValueError:
                 msg = (
-                    'DataArray has no name and cannot be converted to xr.Dataset. '
-                    'please assign a name with  xr.DataArray.rename(new_name)'
+                    "DataArray has no name and cannot be converted to xr.Dataset. "
+                    "please assign a name with  xr.DataArray.rename(new_name)"
                 )
                 raise ValueError(msg)
 
-
         # function will return an error if the file exists and overwrite is False
         if os.path.exists(sname) and not overwrite:
-            print(f'File already exists: {sname}')
+            print(f"File already exists: {sname}")
             return sname
 
-        if compression == 'int16':
-            encoding = get_dataset_compression_encoding(ds, max_percentile=max_percentile)
-        elif compression == 'zip':
-            encoding = {k: {'complevel': 4, 'zlib': True} for k in ds}
+        if compression == "int16":
+            encoding = get_dataset_compression_encoding(
+                ds, max_percentile=max_percentile
+            )
+        elif compression == "zip":
+            encoding = {k: {"complevel": 4, "zlib": True} for k in ds}
         else:
             encoding = {k: {} for k in ds}
-        
+
         ds.to_netcdf(sname, encoding=encoding, **kwargs)
         return sname
-    
+
 
 def save_dataset_with_compression(ds, sname, max_percentile=99.999):
     """
-    Save a dataset with lossy compression. 
+    Save a dataset with lossy compression.
 
-    Here we automatically determine the offset and scale factor of the dataset. 
-    The min and max values of the dataset are set to the 0.001 and 99.999th 
-    percentiles respectively. The data is then scaled based on the range and 
-    the maximum size of a 16-bit integer. Further, the file is compressed with 
+    Here we automatically determine the offset and scale factor of the dataset.
+    The min and max values of the dataset are set to the 0.001 and 99.999th
+    percentiles respectively. The data is then scaled based on the range and
+    the maximum size of a 16-bit integer. Further, the file is compressed with
     zlib to a compression level of 1 (fast speed with minimal difference between
     higher values).
 
@@ -82,9 +87,9 @@ def save_dataset_with_compression(ds, sname, max_percentile=99.999):
     sname: string
         The destination file name of the dataset
     max_percentile: float
-        The upper limit of the data. If you set this to 100, 
-        you will use the maximum and minimum values to establish the range. 
-        However, you will lose precision by doing this. Recommended to set 
+        The upper limit of the data. If you set this to 100,
+        you will use the maximum and minimum values to establish the range.
+        However, you will lose precision by doing this. Recommended to set
         this to 99.999 to eliminate outliers in your data and gain precision
 
     Returns
@@ -97,11 +102,11 @@ def save_dataset_with_compression(ds, sname, max_percentile=99.999):
 
 def get_dataset_compression_encoding(ds, max_percentile=99.999):
     """
-    Creates the encoding to compress data. 
-    
-    WARNING: 
-        this loses precision. If your range is large and your 
-        precision is important, then do not use this method. 
+    Creates the encoding to compress data.
+
+    WARNING:
+        this loses precision. If your range is large and your
+        precision is important, then do not use this method.
 
     Parameters
     ----------
@@ -112,8 +117,8 @@ def get_dataset_compression_encoding(ds, max_percentile=99.999):
     encoding = {}
     for k in ds:
         encoding[k] = get_int16_compression_encoding(
-            ds[k],
-            max_percentile=max_percentile)
+            ds[k], max_percentile=max_percentile
+        )
     return encoding
 
 
@@ -124,9 +129,9 @@ def get_int16_compression_encoding(da, max_percentile=99.999):
 def get_int_encoding(da, n=16, max_percentile=100):
     """
     Calculate encoing offset and scale factor for int conversion
-    I recommend int16 for a balance between good compression and 
-    preservation of precision. 
-    
+    I recommend int16 for a balance between good compression and
+    preservation of precision.
+
     Parameters
     ----------
     da: xr.DataArray
@@ -135,7 +140,7 @@ def get_int_encoding(da, n=16, max_percentile=100):
         integer bit number [8, 16, 32]
         recommend 16
     max_percentile: float [100]
-        Value to set the maximum limit of the data. 
+        Value to set the maximum limit of the data.
         I recommend values greater than 99.99
     """
 
@@ -143,38 +148,48 @@ def get_int_encoding(da, n=16, max_percentile=100):
 
     if not is_numeric_dtype(da.dtype):
         return {}
-    
+
     if max_percentile == 100:
         vmin = da.min().values
         vmax = da.max().values
     elif max_percentile < 100:
         quantile = max_percentile / 100
-        q = [1 - quantile, quantile] 
+        q = [1 - quantile, quantile]
         vmin, vmax = da.quantile(q).values
     else:
-        raise ValueError('max_percentile must be <= 100')
-        
+        raise ValueError("max_percentile must be <= 100")
+
     n_ints = 2**n
     max_scaled = n_ints / 2 - 1
     min_scaled = 1 - n_ints / 2
     scaling_range = max_scaled - min_scaled
-    
+
     # stretch/compress data to the available packed range
     scale_factor = (vmax - vmin) / scaling_range
 
     # translate the range to be symmetric about zero
     add_offset = vmin + max_scaled * scale_factor
-    
+
     # fill_value is 1 less than the minimum scaled value
     fill_value = min_scaled - 1
-    
+
     encoding = dict(
         _FillValue=fill_value,
         add_offset=add_offset,
         scale_factor=scale_factor,
         complevel=1,
-        zlib=True, 
-        dtype=f'int{n}',
+        zlib=True,
+        dtype=f"int{n}",
     )
 
     return encoding
+
+
+def zip_folder(input_dir, output_file):
+    import shutil
+
+    shutil.make_archive(
+        output_file.replace(".zip", ""),
+        "zip",
+        input_dir,
+    )
--- all_my_code/files/utils.py
+++ all_my_code/files/utils.py
@@ -1,12 +1,4 @@
-import os 
-
-
-def zip_folder(input_dir, output_file):
-    import shutil
-    shutil.make_archive(
-        output_file.replace('.zip', ''),
-        'zip',
-        input_dir,)
+import os
 
 
 def move_file_to_parent(fname, levels=1):
@@ -15,26 +7,25 @@ def move_file_to_parent(fname, levels=1):
 
     Used to move files from a zipped folder to the parent directory
     """
-    import os 
     from pathlib import Path as posixpath
 
     old_path = posixpath(fname)
 
     name = old_path.name
     new_path = old_path.parent.parent / name
-    
+
     old_path.rename(new_path)
-    
+
     return str(new_path)
 
 
 def change_file_permissions(path, permission=774):
-    """ 
-    Give group permission to a file or directory 
+    """
+    Give group permission to a file or directory
     """
     from numpy import ndarray
-    import os
     from pathlib import Path as posixpath
+
     perm = int(str(permission), base=8)
 
     if isinstance(path, str):
@@ -49,12 +40,12 @@ def change_file_permissions(path, permission=774):
                 f.parent.chmod(perm)
             except:
                 pass
-            
+
 
 def get_fnames_recursive_search(basedir, include=[], exclude=[]):
     """
     Search and match file names in a directory (recursive)
-    
+
     Parameters
     ----------
     basedir: str (must exist as a path)
@@ -63,13 +54,11 @@ def get_fnames_recursive_search(basedir, include=[], exclude=[]):
         the string patterns that must occur in the files you're looking for
     exclude: list of str
         string patterns you would like to exclude from the filenames
-        
-    Returns 
+
+    Returns
     -------
     A list of file names with the full path
     """
-    import os
-    import re
     from numpy import sort
 
     flist = []
@@ -78,20 +67,22 @@ def get_fnames_recursive_search(basedir, include=[], exclude=[]):
             if all([p in fname for p in include]):
                 has_excluded = [s in fname for s in exclude]
                 if not any(has_excluded):
-                    flist += os.path.join(path, fname),
+                    flist += (os.path.join(path, fname),)
 
     flist = sort(flist)
     return flist
 
 
 def is_pathname_valid(pathname: str) -> bool:
-    '''
+    """
     `True` if the passed pathname is a valid pathname for the current OS;
     `False` otherwise.
-    '''
+    """
     # If this pathname is either not a string or is but is empty, this pathname
     # is invalid.
-    import errno, os, sys
+    import errno
+    import sys
+
     ERROR_INVALID_NAME = 123
 
     try:
@@ -107,9 +98,12 @@ def is_pathname_valid(pathname: str) -> bool:
         # Directory guaranteed to exist. If the current OS is Windows, this is
         # the drive to which Windows was installed (e.g., the "%HOMEDRIVE%"
         # environment variable); else, the typical root directory.
-        root_dirname = os.environ.get('HOMEDRIVE', 'C:') \
-            if sys.platform == 'win32' else os.path.sep
-        assert os.path.isdir(root_dirname)   # ...Murphy and her ironclad Law
+        root_dirname = (
+            os.environ.get("HOMEDRIVE", "C:")
+            if sys.platform == "win32"
+            else os.path.sep
+        )
+        assert os.path.isdir(root_dirname)  # ...Murphy and her ironclad Law
 
         # Append a path separator to this directory if needed.
         root_dirname = root_dirname.rstrip(os.path.sep) + os.path.sep
@@ -138,14 +132,14 @@ def is_pathname_valid(pathname: str) -> bool:
             #   * Under most POSIX-compatible OSes, "ENAMETOOLONG".
             #   * Under some edge-case OSes (e.g., SunOS, *BSD), "ERANGE".
             except OSError as exc:
-                if hasattr(exc, 'winerror'):
+                if hasattr(exc, "winerror"):
                     if exc.winerror == ERROR_INVALID_NAME:
                         return False
                 elif exc.errno in {errno.ENAMETOOLONG, errno.ERANGE}:
                     return False
     # If a "TypeError" exception was raised, it almost certainly has the
     # error message "embedded NUL character" indicating an invalid pathname.
-    except TypeError as exc:
+    except TypeError:
         return False
     # If no exception was raised, all path components and hence this
     # pathname itself are valid. (Praise be to the curmudgeonly python.)
@@ -158,10 +152,10 @@ def is_pathname_valid(pathname: str) -> bool:
 
 
 def is_path_creatable(pathname: str) -> bool:
-    '''
+    """
     `True` if the current user has sufficient permissions to create the passed
     pathname; `False` otherwise.
-    '''
+    """
     # Parent directory of the passed path. If empty, we substitute the current
     # working directory (CWD) instead.
     dirname = os.path.dirname(pathname) or os.getcwd()
@@ -169,17 +163,18 @@ def is_path_creatable(pathname: str) -> bool:
 
 
 def is_path_exists_or_creatable(pathname: str) -> bool:
-    '''
+    """
     `True` if the passed pathname is a valid pathname for the current OS _and_
     either currently exists or is hypothetically creatable; `False` otherwise.
 
     This function is guaranteed to _never_ raise exceptions.
-    '''
+    """
     try:
         # To prevent "os" module calls from raising undesirable exceptions on
         # invalid pathnames, is_pathname_valid() is explicitly called first.
         return is_pathname_valid(pathname) and (
-            os.path.exists(pathname) or is_path_creatable(pathname))
+            os.path.exists(pathname) or is_path_creatable(pathname)
+        )
     # Report failure on non-fatal filesystem complaints (e.g., connection
     # timeouts, permissions issues) implying this path to be inaccessible. All
     # other exceptions are unrelated fatal issues and should not be caught here.
