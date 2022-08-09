0412b53ea9d96e0ca3ccdf8a025d15bae416e100
lukegre <lukegre@gmail.com>
2022-08-09 10:58:10 +0200

    fixed circular import

--- all_my_code/__init__.py
+++ all_my_code/__init__.py
@@ -1,3 +1,16 @@
+import logging
+
+# setting up the logging
+logger = logging.Logger("AMC", level=logging.WARNING)
+while len(logger.handlers) > 0:
+    logger.removeHandler(logger.handlers[0])
+
+formatter = logging.Formatter("[%(name)s] %(message)s")
+handler = logging.StreamHandler(sys.stderr)
+handler.setFormatter(formatter)
+handler.setLevel(logging.WARNING)
+logger.addHandler(handler)
+
 from pkg_resources import DistributionNotFound, get_distribution
 
 # from . import _xarray_stats  # xr.accessors should not be visible
@@ -16,7 +29,6 @@ from .files.download import download_file
 from .munging import date_utils
 
 import sys
-import logging
 
 try:
     __version__ = get_distribution("all_my_code").version
@@ -24,16 +36,6 @@ except DistributionNotFound:
     __version__ = "version_undefined"
 del get_distribution, DistributionNotFound
 
-# setting up the logging
-logger = logging.Logger("AMC", level=logging.WARNING)
-while len(logger.handlers) > 0:
-    logger.removeHandler(logger.handlers[0])
-
-formatter = logging.Formatter("[%(name)s] %(message)s")
-handler = logging.StreamHandler(sys.stderr)
-handler.setFormatter(formatter)
-handler.setLevel(logging.WARNING)
-logger.addHandler(handler)
 logger.warning(f"version: {__version__}")
 
 data = datasets._amc_Data()
